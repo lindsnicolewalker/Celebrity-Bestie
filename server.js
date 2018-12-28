6d5fa0eb802baec86fbf27c8a42a6e88f22f5962
lindsnicolewalker <linds.nicolewalker@gmail.com>
2018-12-27 18:46:53 -0800

    start listening on port in server.js

--- server.js
+++ server.js
@@ -19,3 +19,8 @@ app.use(bodyParser.text());
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
+// Start listening on PORT
+app.listen(PORT, function () {
+    console.log('Friend Finder app is listening on PORT: ' + PORT);
+  });
+  
