d9736768c74b2ced388da57c06a1411203c68f4d
lindsnicolewalker <linds.nicolewalker@gmail.com>
2018-12-27 18:43:34 -0800

    configure the express application, use localhost for setup until heroku

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-
+// Configure the Express application, use localhost for setup, later heroku will use process.env.port
+var app = express();
+var PORT = process.env.PORT || 8080;
